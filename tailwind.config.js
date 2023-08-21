c0e72a48a00b8d3c99eb9a5dcb18856066d1d090
linder3hs <linderhassinger00@gmail.com>
2023-08-21 13:18:10 -0500

    fix: mobile version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        primary: '#1DA1F2',
-        info: '#1E97E1'
+        primary: "#1DA1F2",
+        info: "#1E97E1",
       },
     },
     fontFamily: {
-      roboto: ['Roboto', 'sans-serif'],
+      roboto: ["Roboto", "sans-serif"],
     },
   },
 };
